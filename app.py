7db0d139013374b7719d45b389789b4c756647ea
Ashu Sharma <ashu.sharma@salesforce.com>
2024-02-24 17:16:00 +0530

    Adding Null check

--- app.py
+++ app.py
@@ -2,37 +2,39 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def predict():
     # Get input features from request
+    risk_score = 0.0
     data = request.json
-    KIDSDRIV = data['KIDSDRIV']
-    AGE = data['AGE']
-    HOMEKIDS = data['HOMEKIDS']
-    YOJ = data['YOJ']
-    INCOME = data['INCOME']
-    PARENT1 = data['PARENT1']
-    HOME_VAL = data['HOME_VAL']
-    MSTATUS = data['MSTATUS']
-    GENDER = data['GENDER']
-    EDUCATION = data['EDUCATION']
-    OCCUPATION = data['OCCUPATION']
-    TRAVTIME = data['TRAVTIME']
-    CAR_USE = data['CAR_USE']
-    BLUEBOOK = data['BLUEBOOK']
-    TIF = data['TIF']
-    CAR_TYPE = data['CAR_TYPE']
-    RED_CAR = data['RED_CAR']
-    OLDCLAIM = data['OLDCLAIM']
-    CLM_FREQ = data['CLM_FREQ']
-    REVOKED = data['REVOKED']
-    MVR_PTS = data['MVR_PTS']
-    CLM_AMT = data['CLM_AMT']
-    CAR_AGE = data['CAR_AGE']
-    URBANICITY = data['URBANICITY']
+    if data:
+        KIDSDRIV = data['KIDSDRIV']
+        AGE = data['AGE']
+        HOMEKIDS = data['HOMEKIDS']
+        YOJ = data['YOJ']
+        INCOME = data['INCOME']
+        PARENT1 = data['PARENT1']
+        HOME_VAL = data['HOME_VAL']
+        MSTATUS = data['MSTATUS']
+        GENDER = data['GENDER']
+        EDUCATION = data['EDUCATION']
+        OCCUPATION = data['OCCUPATION']
+        TRAVTIME = data['TRAVTIME']
+        CAR_USE = data['CAR_USE']
+        BLUEBOOK = data['BLUEBOOK']
+        TIF = data['TIF']
+        CAR_TYPE = data['CAR_TYPE']
+        RED_CAR = data['RED_CAR']
+        OLDCLAIM = data['OLDCLAIM']
+        CLM_FREQ = data['CLM_FREQ']
+        REVOKED = data['REVOKED']
+        MVR_PTS = data['MVR_PTS']
+        CLM_AMT = data['CLM_AMT']
+        CAR_AGE = data['CAR_AGE']
+        URBANICITY = data['URBANICITY']
 
-    # Call the predict_risk method of RiskPredictor class
-    risk_score = predict_risk(KIDSDRIV, AGE, HOMEKIDS, YOJ, INCOME, PARENT1, HOME_VAL, MSTATUS, GENDER, EDUCATION, OCCUPATION, TRAVTIME, CAR_USE, BLUEBOOK, TIF, CAR_TYPE, RED_CAR, OLDCLAIM, CLM_FREQ, REVOKED, MVR_PTS, CLM_AMT, CAR_AGE, URBANICITY)
+        # Call the predict_risk method of RiskPredictor class
+        risk_score = predict_risk(KIDSDRIV, AGE, HOMEKIDS, YOJ, INCOME, PARENT1, HOME_VAL, MSTATUS, GENDER, EDUCATION, OCCUPATION, TRAVTIME, CAR_USE, BLUEBOOK, TIF, CAR_TYPE, RED_CAR, OLDCLAIM, CLM_FREQ, REVOKED, MVR_PTS, CLM_AMT, CAR_AGE, URBANICITY)
 
     # Return the output as JSON
     return jsonify({'risk_score': risk_score})
